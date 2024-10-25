ed53dc8d3bddf7605d836683b4c1e3cecc83d808
mzf11125 <77999453+mzf11125@users.noreply.github.com>
2024-10-25 11:28:53 +0700

    add main

--- datatypes/src/main/Main.java
+++ datatypes/src/main/Main.java
@@ -1,7 +1,12 @@
 package main;
 
 public class Main {
-		
+	private int nomor;
+	private String nama;
+	
+	
+	
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
